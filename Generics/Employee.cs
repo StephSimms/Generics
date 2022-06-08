73d015a3f2ac71c2f5f8a87f33c1d80e4e909f56
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2022-06-08 18:09:37 -0300

    Generics Assignment

--- Generics/Employee.cs
+++ Generics/Employee.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Generics
 {
     public class Employee<T>
     {
         public List<T> Things { get; set; }
-        public List<T> fName { get; set; }
-        public List<T> lName { get; set; }
+        
         
     }
 }
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Generics
 {
@@ -7,12 +11,17 @@ namespace Generics
         static void Main(string[] args)
         {
 
-            Employee<Things> employee = new Employee<Things>();
-            Employee employee = new Employee();
+            Employee<string> emp1 = new Employee<string>();
+            emp1.Things = new List<string>() { "one hundred ", "two hundred ", "three hundred ", "four hundred ", "five hundred " };
 
-            employee.fName = new List<string>() { " Steph " , " Spencer ", " Devlan "};
-            employee.lName = new List<string>() { " Simms " , " Simms ", " Simms "};
-            employee.ID = new List<int>() { 7, 13, 21 };
+            Employee<int> emp2 = new Employee<int>();
+            emp2.Things = new List<int>() { 100, 200, 300, 400, 500 };
+
+            for (int i = 0; i < emp1.Things.Count(); i++) 
+            {
+                Console.WriteLine(emp1.Things[i] + "and " + emp2.Things[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
