0c62448a7f3fd4d2e911520659a7ee450eb8c19b
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2022-06-08 15:47:57 -0300

    Add project files.

---
+++ Generics/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class Employee<T>
+    {
+        public List<T> Things { get; set; }
+        public List<T> fName { get; set; }
+        public List<T> lName { get; set; }
+        
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee<Things> employee = new Employee<Things>();
+            Employee employee = new Employee();
+
+            employee.fName = new List<string>() { " Steph " , " Spencer ", " Devlan "};
+            employee.lName = new List<string>() { " Simms " , " Simms ", " Simms "};
+            employee.ID = new List<int>() { 7, 13, 21 };
+        }
+    }
+}
